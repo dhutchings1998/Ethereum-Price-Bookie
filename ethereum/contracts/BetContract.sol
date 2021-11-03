4418b347199187f9b2ed40cfaeb6c86bd6429fa1
dhutchings1998 <dhutchings1998@gmail.com>
2021-11-02 22:54:08 -0400

    Adding tests and Contract updates

--- ethereum/contracts/BetContract.sol
+++ ethereum/contracts/BetContract.sol
@@ -1,32 +1,58 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract BetContractFactory {
     address manager;
     mapping(address => BetContract[]) public addressToContracts;
     mapping(address => bool) public addressExists;
-    mapping(address => uint) public addressToPayout;
+    mapping(address => uint256) public addressToPayout;
 
     event NewContract(address creator);
     event PayoutWinnings(address recipient);
-    
+
     constructor() {
         manager = msg.sender;
     }
-    
+
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    function createBetContract(uint targetGuess, uint marginOfError, uint numDays, uint startTimestamp, uint endTimestamp) public payable {
-        require(msg.value > 0, "Need to contribute ether in order to place bet");
-        require(msg.value < address(this).balance / 5, "Can't bet more than 20% of the value of the pot");
-        require(numDays >= 1, "Bet needs to be placed at least 1 day in the future");
+
+    function fundBalance() external payable {}
+
+    function createBetContract(
+        uint256 targetGuess,
+        uint256 marginOfError,
+        uint256 numDays,
+        uint256 startTimestamp
+    ) public payable {
+        require(
+            msg.value > 0,
+            "Need to contribute ether in order to place bet"
+        );
+        require(
+            msg.value < address(this).balance / 5,
+            "Can't bet more than 20% of the value of the pot"
+        );
+        require(
+            numDays >= 1,
+            "Bet needs to be placed at least 1 day in the future"
+        );
         require(marginOfError <= 50, "Margin of error must be less than $50");
-        
-        BetContract newContract = new BetContract(msg.sender, manager, msg.value, targetGuess, marginOfError, numDays, startTimestamp, endTimestamp);
-        
+
+        BetContract newContract = new BetContract(
+            msg.sender,
+            manager,
+            msg.value,
+            targetGuess,
+            marginOfError,
+            numDays,
+            startTimestamp
+        );
+
         if (addressExists[msg.sender] == true) {
             addressToContracts[msg.sender].push(newContract);
         } else {
@@ -35,57 +61,73 @@ contract BetContractFactory {
         }
         emit NewContract(msg.sender);
     }
-    
-    function calculatePayout(address bettor, uint[] memory correctPrices) public restricted {
+
+    function calculatePayout(address bettor, uint256[] memory correctPrices)
+        public
+        restricted
+    {
         BetContract[] storage liveContracts = addressToContracts[bettor];
 
-        for (uint i=0; i < liveContracts.length; i++) {
+        for (uint256 i = 0; i < liveContracts.length; i++) {
             BetContract bc = liveContracts[i];
-            require(bc.potentialReward() > 0, "Potential reward must be greater than $0");
-            
-            uint currentTimestamp = block.timestamp;
+            require(
+                bc.potentialReward() > 0,
+                "Potential reward must be greater than $0"
+            );
+
+            uint256 currentTimestamp = block.timestamp;
 
             if (bc.didWin(correctPrices[i], currentTimestamp) == true) {
                 addressToPayout[bettor] += bc.potentialReward();
             }
-            
+
             if (currentTimestamp > bc.endTimestamp()) {
                 removeContract(i, liveContracts);
             }
         }
     }
-    
+
     function payoutWinnings(address recipient) public restricted {
         payable(recipient).transfer(addressToPayout[recipient]);
         addressToPayout[recipient] = 0;
         emit PayoutWinnings(recipient);
     }
-    
-    function removeContract(uint index, BetContract[] storage bets) internal {
-          require(index < bets.length, "Index out of range");
-          bets[index] = bets[bets.length-1];
-          bets.pop();
+
+    function removeContract(uint256 index, BetContract[] storage bets)
+        internal
+    {
+        require(index < bets.length, "Index out of range");
+        bets[index] = bets[bets.length - 1];
+        bets.pop();
     }
 }
 
 contract BetContract {
+    using SafeMath for uint256;
     address public bettor; // address
     address public manager;
-    uint public betAmount; // wei
-    uint public targetGuess; // 
-    uint public marginOfError; 
-    uint public numDays; // int
-    uint public potentialReward; // wei
-    uint public startTimestamp; // timestamp
-    uint public endTimestamp; // timestamp
+    uint256 public betAmount; // wei
+    uint256 public targetGuess; //
+    uint256 public marginOfError;
+    uint256 public numDays; // int
+    uint256 public potentialReward; // wei
+    uint256 public startTimestamp; // timestamp
+    uint256 public endTimestamp; // timestamp
 
-    
     modifier restricted() {
         require(msg.sender == manager, "Only manager can call this function");
         _;
     }
-    
-    constructor(address _bettor, address _manager, uint _betAmount, uint _targetGuess, uint _marginOfError, uint _numDays, uint _startTimestamp, uint _endTimestamp) {
+
+    constructor(
+        address _bettor,
+        address _manager,
+        uint256 _betAmount,
+        uint256 _targetGuess,
+        uint256 _marginOfError,
+        uint256 _numDays,
+        uint256 _startTimestamp
+    ) {
         bettor = _bettor;
         manager = _manager;
         betAmount = _betAmount;
@@ -93,19 +135,27 @@ contract BetContract {
         marginOfError = _marginOfError;
         numDays = _numDays;
         startTimestamp = _startTimestamp;
-        endTimestamp = _endTimestamp;
         potentialReward = _betAmount;
+        endTimestamp = _startTimestamp.add(_numDays.mul(86400000));
     }
-    
-    function setPotentialReward(uint payout) public restricted {
+
+    function setPotentialReward(uint256 payout) public restricted {
         potentialReward = payout;
     }
-    
-    function didWin(uint correctPrice, uint currentTimestamp) public view returns (bool) {
-        uint lowerBound = targetGuess - marginOfError;
-        uint upperBound = targetGuess + marginOfError;
-        
-        if (correctPrice <= upperBound && correctPrice >= lowerBound && currentTimestamp > endTimestamp) {
+
+    function didWin(uint256 correctPrice, uint256 currentTimestamp)
+        public
+        view
+        returns (bool)
+    {
+        uint256 lowerBound = targetGuess - marginOfError;
+        uint256 upperBound = targetGuess + marginOfError;
+
+        if (
+            correctPrice <= upperBound &&
+            correctPrice >= lowerBound &&
+            currentTimestamp > endTimestamp
+        ) {
             return true;
         }
         return false;
--- test/BetContract.test.js
+++ test/BetContract.test.js
@@ -4,6 +4,7 @@ const web3 = new Web3(ganache.provider());
 
 const compiledBetFactory = require("../bin/ethereum/contracts/BetContractFactory.json");
 const compiledBetContract = require("../bin/ethereum/contracts/BetContract.json");
+const { isTargetLikeServerless } = require("next/dist/server/config");
 
 describe("Bet Contract", () => {
 	let accounts;
@@ -33,9 +34,6 @@ describe("Bet Contract", () => {
 		// Fetch new bet contract address and configure with web3
 		contractAddress = await betFactory.methods.addressToContracts(accounts[1], 0).call();
 		betContract = await new web3.eth.Contract(compiledBetContract.abi, contractAddress);
-
-        console.log(await betContract.methods.startTimestamp().call())
-        console.log(await betContract.methods.endTimestamp().call())
 	});
 
 	test("deploys a factory", () => {
@@ -63,7 +61,7 @@ describe("Bet Contract", () => {
 	});
 
 	test("throws error during bet contract creation when bet amount exceeds 20% of value of the pot", async () => {
-        try {
+		try {
 			await betFactory.methods
 				.createBetContract(4500, 40, 3, Date.now())
 				.send({ from: accounts[1], gas: "5000000", value: "55000000000000000" });
@@ -71,10 +69,10 @@ describe("Bet Contract", () => {
 		} catch (err) {
 			expect(err).toBeTruthy();
 		}
-    });
+	});
 
-    test("throws error during bet contract creation when numDays < 1", async () => {
-        try {
+	test("throws error during bet contract creation when numDays < 1", async () => {
+		try {
 			await betFactory.methods
 				.createBetContract(4500, 40, 0, Date.now())
 				.send({ from: accounts[1], gas: "5000000", value: "100" });
@@ -82,10 +80,10 @@ describe("Bet Contract", () => {
 		} catch (err) {
 			expect(err).toBeTruthy();
 		}
-    });
+	});
 
-    test("throws error during bet contract creation when margin of error > 50", async () => {
-        try {
+	test("throws error during bet contract creation when margin of error > 50", async () => {
+		try {
 			await betFactory.methods
 				.createBetContract(4500, 51, 1, Date.now())
 				.send({ from: accounts[1], gas: "5000000", value: "100" });
@@ -93,5 +91,42 @@ describe("Bet Contract", () => {
 		} catch (err) {
 			expect(err).toBeTruthy();
 		}
-    });
+	});
+
+	test("setting potential reward for bet contract updates potentialReward variable", async () => {
+		await betContract.methods.setPotentialReward("1000").send({ from: accounts[0], gas: "5000000" });
+		const potentialReward = await betContract.methods.potentialReward().call();
+
+		expect(potentialReward).toBe("1000");
+	});
+
+	test("only manager can call setPotentialReward", async () => {
+		try {
+			await betContract.methods.setPotentialReward("1000").send({ from: accounts[1], gas: "5000000" });
+			expect(false);
+		} catch (err) {
+			expect(err).toBeTruthy();
+		}
+	});
+
+	test("endTimestamp is set correctly when bet contract is created", async () => {
+		await betFactory.methods
+			.createBetContract(4500, 40, 1, Date.now())
+			.send({ from: accounts[2], gas: "5000000", value: "1000" });
+		contractAddress1 = await betFactory.methods.addressToContracts(accounts[2], 0).call();
+		betContract1 = await new web3.eth.Contract(compiledBetContract.abi, contractAddress1);
+		startTimestamp1 = await betContract1.methods.startTimestamp().call();
+		endTimestamp1 = await betContract1.methods.endTimestamp().call();
+
+		await betFactory.methods
+			.createBetContract(4500, 40, 2, Date.now())
+			.send({ from: accounts[3], gas: "5000000", value: "1000" });
+		contractAddress2 = await betFactory.methods.addressToContracts(accounts[3], 0).call();
+		betContract2 = await new web3.eth.Contract(compiledBetContract.abi, contractAddress2);
+		startTimestamp2 = await betContract2.methods.startTimestamp().call();
+		endTimestamp2 = await betContract2.methods.endTimestamp().call();
+
+		expect(parseInt(endTimestamp1)).toBe(parseInt(startTimestamp1) + 86400000);
+		expect(parseInt(endTimestamp2)).toBe(parseInt(startTimestamp2) + 86400000 * 2);
+	});
 });
